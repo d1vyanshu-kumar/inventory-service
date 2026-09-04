cd0e7ea3c495ddbae704d689674d5e111b1e7dbf
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2026-09-04 12:15:00 +0530

    feat: add findBySkuCode query method to inventory repository

--- src/main/java/com/divyanshuLearn/Microservices/inventory/repository/InventoryRepository.java
+++ src/main/java/com/divyanshuLearn/Microservices/inventory/repository/InventoryRepository.java
@@ -2,9 +2,9 @@ package com.divyanshuLearn.Microservices.inventory.repository;
 
 import com.divyanshuLearn.Microservices.inventory.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    // sprind data jpa is automatically going to write query for this method.
-    // so whenever i call this method this will execute the method and send the response.
     boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
